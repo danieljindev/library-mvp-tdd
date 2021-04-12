77c01ce4c22613b44f607f2b8da21d6ef083c25a
Daniel Jin <danieljin123@outlook.com>
2021-04-13 00:06:24 +0300

    mvp done

---
+++ README.md
@@ -0,0 +1,7 @@
+TDD approach for Laravel + Vue Development(Library MVP)
+
+# Tech Stack
+- Laravel
+- Vue.js
+- Tailwind CSS
+- Feature and Unit Test
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::post('/books', 'BooksController@store');
+Route::patch('/books/{book}', 'BooksController@update');
+Route::delete('/books/{book}', 'BooksController@destroy');
+
+Route::get('/authors/create', 'AuthorsController@create');
+Route::post('/authors', 'AuthorsController@store');
+
+Route::post('/checkout/{book}', 'CheckoutBookController@store');
+Route::post('/checkin/{book}', 'CheckinBookController@store');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
